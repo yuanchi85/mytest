cf5e2a2e6073a89f79e12dad42659600d59834d3
yuanchi85 <yuanchi85@hotmail.com>
2021-04-19 18:37:51 +0800

    new project

---
+++ account-email/src/main/java/com/ycy/mvnprojs/account/email/AccountEmailService.java
@@ -0,0 +1,6 @@
+package com.ycy.mvnprojs.account.email;
+
+public interface AccountEmailService {
+    void sendMail( String to, String subject, String htmlText )
+            throws AccountEmailException;
+}
